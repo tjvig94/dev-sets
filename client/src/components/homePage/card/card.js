0329316bd65114250ecdd5bc5d60975e734801c4
Chad Courtney <Chadcourtney567@gmail.com>
2021-05-13 18:15:24 -0400

    color scheme change

--- client/src/components/homePage/card/card.js
+++ client/src/components/homePage/card/card.js
@@ -4,13 +4,11 @@ import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import { grey } from '@material-ui/core/colors';
+import { brown } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import { Modal, Button } from "@material-ui/core";
@@ -38,7 +36,7 @@ const useStyles = makeStyles((theme) => ({
         transform: 'rotate(180deg)',
     },
     avatar: {
-        backgroundColor: grey[500],
+        backgroundColor: brown[500],
     },
     modal: {
         display: 'flex',
@@ -51,16 +49,13 @@ const useStyles = makeStyles((theme) => ({
         boxShadow: theme.shadows[5],
         padding: theme.spacing(2, 4, 3),
     },
+    style: {
+        backgroundColor: "#6b645d",
+    },
 }));
 
 export default function ContentCard() {
     const classes = useStyles();
-    const [expanded, setExpanded] = React.useState(false);
-
-    const handleExpandClick = () => {
-        setExpanded(!expanded);
-    };
-
     const [open, setOpen] = React.useState(false);
 
     const handleOpen = () => {
@@ -72,12 +67,14 @@ export default function ContentCard() {
     };
 
 
+
     return (
         <Card className={classes.root}>
             <CardHeader
+                className={classes.style}
                 avatar={
                     <Avatar aria-label="recipe" className={classes.avatar}>
-                        R
+                        M
           </Avatar>
                 }
                 action={
@@ -89,10 +86,13 @@ export default function ContentCard() {
             />
             <CardMedia
                 className={classes.media}
-                image="/static/images/cards/paella.jpg"
+                image={temp}
                 title="Paella dish"
             />
-            <CardActions disableSpacing>
+            <CardActions
+                className={classes.style}
+                disableSpacing
+            >
                 <IconButton aria-label="add to favorites">
                     <FavoriteIcon />
                 </IconButton>
@@ -124,33 +124,6 @@ export default function ContentCard() {
                 </Modal>
 
             </CardActions>
-            <Collapse in={expanded} timeout="auto" unmountOnExit>
-                <CardContent>
-                    <Typography paragraph>Method:</Typography>
-                    <Typography paragraph>
-                        Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-                        minutes.
-          </Typography>
-                    <Typography paragraph>
-                        Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-                        heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-                        browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-                        and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-                        pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-                        saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-                    <Typography paragraph>
-                        Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-                        without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-                        medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-                        again without stirring, until mussels have opened and rice is just tender, 5 to 7
-                        minutes more. (Discard any mussels that don’t open.)
-          </Typography>
-                    <Typography>
-                        Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>
-                </CardContent>
-            </Collapse>
         </Card>
     );
 }
