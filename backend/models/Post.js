35087e04c94ebcfc0331fb0325ee81e91804bd1b
Joe Lyons <josephjlyons90@gmail.com>
2021-05-13 15:25:45 -0400

    change data type and content type in post model

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const postSchema = new Schema({
     title: { type: String },
     desc: { type: String },
-    image: { type: String },
+    image: { data: Buffer, contentType: String },
     user: { type: String, required: true }
 });
 
