1a7e567d3b315351c8969d2855b98b2ae39f4e21
Tim Vigneau <tjvig94@gmail.com>
2021-05-12 21:36:14 -0400

    Can acquire user data by email from backend

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,13 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 app.use(express.json());
 app.use(express.urlencoded( { extended: true }));
@@ -11,5 +18,9 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/devsets", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
+admin.auth().getUserByEmail("tjvig94@gmail.com").then(res => {
+    console.log(res.toJSON());
+})
 
 app.listen(PORT, () => console.log(`App running on port ${PORT}.`));
