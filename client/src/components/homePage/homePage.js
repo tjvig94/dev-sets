1235efda6f5c9f47bf90eb01a0a85b82244b5a68
Tim Vigneau <tjvig94@gmail.com>
2021-05-17 21:13:34 -0400

    generates posts on homepage

--- client/src/components/homePage/homePage.js
+++ client/src/components/homePage/homePage.js
@@ -3,78 +3,32 @@ import "./homePage.css";
 import { Container, Row } from "@material-ui/core";
 import ContentCard from "./card/card";
 import Form from "./form/form";
+import API from '../../utils/API';
 
 // database query for random
 
-const dummyData = [
-    {
-        title: "The Village",
-        description: "Resident Evil",
-        image: "https://via.placeholder.com/150",
-        id: "1",
-        user: {
-            id: "",
-            name: "John",
-            image: ""
-        }
-    },
-    {
-        title: "Destiny 2",
-        description: "Outer world shooter",
-        image: "https://via.placeholder.com/150",
-        id: "2",
-        user: {
-            id: "",
-            name: "Joe",
-            image: ""
-        },
-    },
-    {
-        title: "League of Legends",
-        description: "pain",
-        image: "https://via.placeholder.com/150",
-        id: "3",
-        user: {
-            id: "",
-            name: "Chad",
-            image: ""
-        }
-    }
-]
-
 function HomePage({ user }) {
     const [cardArray, setCardArray] = useState([]);
+    const [postData, setPostData] = useState([]);
     
     useEffect(() => {
-        setCardArray(dummyData)
-    }, [])
-
+        API.getPosts().then(res => {
+            setPostData(res.data);
+        });
+        setCardArray(postData);
+        
+    }, [postData]);
 
     return (
         <div>
             <Container maxWidth="lg" className="homeContent">
-
                 {cardArray.map(post => (
                     <ContentCard post={post} key={post.id} />
                 ))}
-
-
             </Container>
-        <Form user={user}/>
+            <Form user={user}/>
         </div>
     )
-}
+};
 
 export default HomePage;
-
-
-
-
-//     API.getDogsOfBreed(this.state.search)
-// .then(res => {
-//     if (res.data.status === "error") {
-//       throw new Error(res.data.message);
-//     }
-//     this.setState({ results: res.data.message, error: "" });
-//   })
-//   .catch(err => this.setState({ error: err.message }));
