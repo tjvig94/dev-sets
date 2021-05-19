6b47b34d1ebda3112242bc260a5378a4a7d0f7a9
Patrick DeMarco <pfdemarco@hotmail.com>
2021-05-18 20:59:30 -0400

    a lot of chnages added new routes

--- backend/Controllers/api/index.js
+++ backend/Controllers/api/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const postRoutes = require('./postRoutes');
-
+const profileRoutes = require('./profileRoutes');
 
 router.use('/post', postRoutes)
+router.use('/profile', profileRoutes)
 
 module.exports = router;
