3662e14a61c4b1ebb96185a9d753d671bac9af6c
Chad Courtney <Chadcourtney567@gmail.com>
2021-05-24 18:16:13 -0400

    added snackbar, still working on close method

--- client/src/components/homePage/form/form.js
+++ client/src/components/homePage/form/form.js
@@ -3,8 +3,27 @@ import { Button } from "@material-ui/core";
 import "./form.css";
 import storage from '../../../firebase';
 import axios from 'axios';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        '& > * + *': {
+            marginTop: theme.spacing(2),
+        },
+    },
+}));
 
 function Form({ user }) {
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
 
     const [file, setFile] = useState(null);
     const [title, setTitle] = useState('');
@@ -20,13 +39,25 @@ function Form({ user }) {
         setDesc(event.target.value)
     }
 
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpen(false);
+    };
+    const handleClick = () => {
+        setOpen(true);
+    };
+
+
     const handleSubmit = async (event) => {
         event.preventDefault();
 
         // send image to firebase storage, and get reference url
-        const uploadTask = await storage.ref(`/images/${file.name}`).put(file);     
+        const uploadTask = await storage.ref(`/images/${file.name}`).put(file);
         const imageUrl = await storage.ref('images').child(file.name).getDownloadURL();
-        
+
         // create formdata to send to database 
         const formData = {
             user: user.uid,
@@ -58,7 +89,13 @@ function Form({ user }) {
                         name="image" onChange={handleChange} required />
                 </div>
                 <div className="formButton">
-                    <Button type="submit" variant="contained" >Submit</Button>
+                    <Button type="submit" variant="contained">Submit</Button>
+
+                    <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+                        <Alert onClose={handleClose} severity="success">
+                            Post was successful!
+                </Alert>
+                    </Snackbar>
                 </div>
             </form>
         </div>
