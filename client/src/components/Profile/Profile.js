16c020a4ff5de1b642d40b0b86d313611302c28b
Patrick DeMarco <pfdemarco@hotmail.com>
2021-05-25 12:43:38 -0400

    sexy time

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,6 +1,18 @@
 import React,{useEffect,useState} from 'react'
 //import {UserContext} from '../../App'
 import firebase from 'firebase';
+import { colors } from '@material-ui/core';
+
+
+
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 
 const Profile  = () => {
@@ -8,7 +20,18 @@ const Profile  = () => {
     const [image,setImage] = useState("")
     //const { user } = useContext(UserContext);
     let user = firebase.auth().currentUser;
+
+    const useStyles = makeStyles({
+        root: {
+          maxWidth: 345,
+        },
+        media: {
+          height: 140,
+        },
+      });
     
+    const classes = useStyles();
+
     useEffect(()=>{
        fetch('/api/profile/profilePic',{
            headers:{
@@ -118,60 +141,92 @@ const Profile  = () => {
         // setImage(file)
         
        // setImage(file)
+      // export default function MediaCard() {
+        //const classes = useStyles();
+
     }
-   return (
-       <div style={{maxWidth:"550px",margin:"0px auto"}}>
-           <div style={{
-              margin:"18px 0px",
-               borderBottom:"1px solid grey"
-           }}>
-
-         
-           <div style={{
-               display:"flex",
-               justifyContent:"space-around",
-              
-           }}>
-               <div>
-                   <img style={{width:"160px",height:"160px",borderRadius:"80px"}}
-                   src={user.photoURL}alt="profile"
-                   />
-                 
-               </div>
-               <div>
-                  
-                   <div style={{display:"flex",justifyContent:"space-between",width:"108%"}}>
-                       <h6>{mypics} posts</h6>
- 
-                   </div>
-
-               </div>
-           </div>
-        
-            <div className="file-field input-field" style={{margin:"10px"}}>
-            <div className="btn #64b5f6 white darken-1">
-                <span>Update pic</span>
-                <input type="file" onChange={(e)=>updatePhoto(e.target.files[0])} />//
-            </div>
-            {/* <div className="file-path-wrapper">
-                <input className="file-path validate" type="text" />
-            </div> */}
-            </div>
-            </div>      
-           <div className="gallery">
-               {
-                //    mypics.map(item=>{
-                //        return(
-                //         <img key={item._id} className="item" src={item.photo} alt={item.title}/>  
-                //        )
-                //    })
-               }
-
-           
-           </div>
-       </div>
-   )
-}
-
+     
+      return (
+        <Card className={classes.root}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              image= {user.photoURL} //"/static/images/cards/contemplative-reptile.jpg"
+              title= {user.displayName}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {user.displayName}
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Email address for {user.displayName} is {user.email}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              Share
+            </Button>
+            <Button size="small" color="primary">
+              Learn More
+            </Button>
+          </CardActions>
+        </Card>
+      );
+    }
 
 export default Profile
+
+// return (
+//     <div style={{maxWidth:"550px",margin:"0px auto", color:'white'}}>
+//         <div style={{
+//            margin:"18px 0px",
+//             borderBottom:"1px solid grey"
+           
+//         }}>
+
+      
+//         <div style={{
+//             display:"flex",
+//             justifyContent:"space-around",
+           
+//         }}>
+//             <div>
+//                 <img style={{width:"160px",height:"160px",borderRadius:"80px"}}
+//                 src={user.photoURL}alt="profile"
+//                 />
+              
+//             </div>
+//             <div>
+               
+//                 <div style={{display:"flex",justifyContent:"space-between",width:"108%"}}>
+//                     <h6>{mypics} posts</h6>
+
+//                 </div>
+
+//             </div>
+//         </div>
+     
+//          <div className="file-field input-field" style={{margin:"10px"}}>
+//          <div className="btn #64b5f6 white darken-1">
+//              <span>Update profile pic</span>
+//              <input type="file" onChange={(e)=>updatePhoto(e.target.files[0])} />//
+//          </div>
+//          {/* <div className="file-path-wrapper">
+//              <input className="file-path validate" type="text" />
+//          </div> */}
+//          </div>
+//          </div>      
+//         <div className="gallery">
+//             {
+//              //    mypics.map(item=>{
+//              //        return(
+//              //         <img key={item._id} className="item" src={item.photo} alt={item.title}/>  
+//              //        )
+//              //    })
+//             }
+
+        
+//         </div>
+//     </div>
+// )
